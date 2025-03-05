a5de82a3ef12a21cef5851b7388c98226a761d2b
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-03-05 18:56:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
-//your JS code here. If required.
-let output=document.getElementById('output')
+let output = document.getElementById('output');
+let results = [];
 
-output.innerHTML=`<tr><td>Loading...</td></tr>`
+output.innerHTML = `<tr><td colspan="2">Loading...</td></tr>`;
+
 let promise1 = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            output.innerHTML += `<tr><td>Promise 1</td><td>2</td></tr>`;
+            results.push(`<tr><td>Promise 1</td><td>2</td></tr>`);
             resolve('Promise 1 completed');
         }, 2000);
     });
 };
 
-
 let promise2 = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            output.innerHTML = `<tr><td>Promise 2</td><td>1</td></tr>`;
+            results.push(`<tr><td>Promise 2</td><td>1</td></tr>`);
             resolve('Promise 2 completed');
         }, 1000);
     });
 };
 
-
 let promise3 = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
-            output.innerHTML += `<tr><td>Promise 3</td><td>3</td></tr>`;
-            resolve('Promise 2 completed');
+            results.push(`<tr><td>Promise 3</td><td>3</td></tr>`);
+            resolve('Promise 3 completed');
         }, 3000);
     });
 };
 
-let promise4=()=>{
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-			output.innerHTML += `<tr><td><b>Total</b></td><td>3.006</td></tr>`;
-			resolve('all prmises completed')
-		},3006)
-	})
-}
+let promise4 = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            results.push(`<tr><td><b>Total</b></td><td>3.006</td></tr>`);
+            resolve('All promises completed');
+        }, 3006);
+    });
+};
 
-
-Promise.all([promise1(),promise2(),promise3(),promise4()])
-.then(data=>{
-	 console.log(data);
-})
-.catch(err=>{
-	 console.log(err);
-})
+Promise.all([promise1(), promise2(), promise3(), promise4()])
+    .then(() => {
+        output.innerHTML = results.join('');
+    })
+    .catch(err => {
+        console.log(err);
+    });
